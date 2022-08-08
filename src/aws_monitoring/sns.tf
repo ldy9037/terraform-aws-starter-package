065ef2eaf7e73a7faf6c7702ef13a1e6e06721bb
emadam <ldy9037@naver.com>
2022-08-08 20:18:53 +0900

    feat: sns 관리 권한 정책 추가

--- src/aws_monitoring/sns.tf
+++ src/aws_monitoring/sns.tf
@@ -1,3 +1,46 @@
+locals {
+  sns_topic_arn = var.cloudtrail_enabled && var.sns_topic_enabled ? aws_sns_topic.cloudwatch_alarms_for_cloudtrail[0].arn : ""
+}
+
+data "aws_iam_policy_document" "alarms_sns_policy" {
+  policy_id = "__default_policy_ID"
+
+  statement {
+    actions = [
+      "SNS:GetTopicAttributes",
+      "SNS:SetTopicAttributes",
+      "SNS:AddPermission",
+      "SNS:RemovePermission",
+      "SNS:DeleteTopic",
+      "SNS:Subscribe",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:Publish",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        var.aws_account_id,
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      "${local.sns_topic_arn}",
+    ]
+
+    sid = "__default_statement_ID"
+  }
+}
+
 resource "aws_sns_topic" "cloudwatch_alarms_for_cloudtrail" {
   count = var.cloudtrail_enabled && var.sns_topic_enabled ? 1 : 0
 
@@ -18,4 +61,11 @@ resource "aws_sns_topic_subscription" "slack" {
   topic_arn = aws_sns_topic.cloudwatch_alarms_for_cloudtrail[0].arn
   protocol  = "https"
   endpoint  = "https://global.sns-api.chatbot.amazonaws.com"
+}
+
+resource "aws_sns_topic_policy" "alarms" {
+  count = var.cloudtrail_enabled && var.sns_topic_enabled ? 1 : 0
+
+  arn    = aws_sns_topic.cloudwatch_alarms_for_cloudtrail[0].arn
+  policy = data.aws_iam_policy_document.alarms_sns_policy.json
 }
