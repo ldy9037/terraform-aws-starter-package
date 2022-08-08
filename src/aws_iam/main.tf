b88fa28caeffad7542726a5dd764ed5d76d1e6e8
emadam <ldy9037@naver.com>
2022-08-08 12:03:11 +0900

    feat: iam-group-with-policies 모듈을 사용해 group 생성 그룹과 사용자를 구조화해 local 변수로 저장

--- src/aws_iam/main.tf
+++ src/aws_iam/main.tf
@@ -29,6 +29,20 @@ provider "aws" {
   }
 }
 
+locals {
+  iam_group = {
+    Administrator = {
+      create_group = true
+      user = [
+        module.iam_user["ldy"].iam_user_name,
+      ]
+      policies = [
+        "arn:aws:iam::aws:policy/AdministratorAccess"
+      ] 
+    }
+  }
+}
+
 resource "aws_iam_account_alias" "alias" {
   account_alias = var.iam_account_alias
 }
@@ -45,3 +59,15 @@ module "iam_user" {
   create_iam_user_login_profile = each.value["iam_user_create_login_profile"]
 }
 
+module "iam_group" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
+
+  for_each = local.iam_group
+
+  name = each.key
+
+  create_group = each.value["create_group"]
+
+  custom_group_policy_arns = each.value["policies"]
+  group_users              = each.value["user"]
+}
